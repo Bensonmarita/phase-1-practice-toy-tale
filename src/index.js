ce9a629e71f5ded41d55c87ca0d134d30dd30724
benmarita96@gmail.com <benmarita96@gmail.com>
2022-08-31 01:16:22 -0400

    Assignment completed

--- src/index.js
+++ src/index.js
@@ -13,3 +13,60 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+const collection = document.querySelector("#toy-collection")
+const form = document.querySelector('.add-toy-form')
+//fetching toys
+function toyCollection (arrToys){
+  fetch("  http://localhost:3000/toys")
+  .then(resp=>resp.json())
+}
+//rendering functions
+function render(arrToys){
+  arrToys.forEach(renderOneToy)
+}
+  
+ function renderOneToy(){
+  const cards= document.createElement("div");
+  cards.className= "card"
+
+  const h2 = document.createElement("h2");
+  h2.innerHTML = arrToys.h2;
+
+  const image = document.createElement("img");
+  image.className="toy-avatar";
+  image.src= arrToys.src
+
+  const p = document.createElement("p")
+    p.innerHTML=`${arrToys.likes} likes`
+
+    const buttons = document.createElement("button")
+    buttons.innerHTML = "Like ❤️"
+    buttons.className= "like-btn"
+    buttons.id = arrToys.id
+
+    cards.appendChild(div)
+    div.appendChild(h2);
+    div.appendChild(image);
+    div.appendChild(p)
+    div.appendChild(buttons)
+
+    buttons.addEventListener("click",(
+      function handleLikes(){
+        let moreLikes = parseInt(likes.textContent)
+        likes.textContent=`${moreLikes+1} likes`
+      }
+    ))
+
+    }
+    //event handlers
+    form.addEventListener("submit",(
+      function newToy(e){
+        e.preventDefault();
+        console.log(e.target.elements.name.value);
+        console.log(e.target.elements.image.value);
+        $TARGET = e.target;
+      }
+    ))
+    //invoking our function
+    toyCollection().then(render)
+  
